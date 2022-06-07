21a39b4b1a747506f0733ecd051c382cdafcd5d1
hasanrafi69 <hasanrafi69@gmail.com>
2022-06-07 14:49:47 +0600

    stripe added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 app.use(cors());
 app.use(express.json());
 // ======Port ========
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5000
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.evg4w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
