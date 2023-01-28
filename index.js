21ae5b199364c4492c963a080710928f52a3d077
Md. Hasan Mia <hasanrafi69@gmail.com>
2023-01-28 13:43:05 +0600

    added env

--- index.js
+++ index.js
@@ -15,12 +15,8 @@ app.use(express.json());
 const port = process.env.PORT || 5000
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.evg4w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	serverApi: ServerApiVersion.v1
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.evg4w.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 //==============================//
